6e40b84d490299a62fba33f4ece2d070e8939db8
Amanda Claproth <amanda.claproth@gmail.com>
2020-10-12 15:39:12 -0700

    auth done

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const logger = require('morgan');
 
 const app = express();
 
+
 require('dotenv').config();
 require('./config/database');
+
+
 
 app.use(logger('dev'));
 
@@ -14,6 +17,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/users', require('./routes/api/users'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
